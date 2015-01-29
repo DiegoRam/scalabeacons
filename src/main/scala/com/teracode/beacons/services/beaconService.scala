cca70538d0674c552ce5f89a4d01f6abd3247f8f
diegoram <ddarkheart@gmail.com>
2015-01-29 16:35:08 -0300

    fix beacon endpoints

--- src/main/scala/com/teracode/beacons/services/beaconService.scala
+++ src/main/scala/com/teracode/beacons/services/beaconService.scala
@@ -13,30 +13,28 @@ trait BeaconService extends HttpService {
 
   @ApiOperation(value = "Updated beacon", notes = "This can only be done by the logged in user.", nickname = "updateUser", httpMethod = "PUT")
   @ApiImplicitParams(Array(
-    new ApiImplicitParam(name = "username", value = "ID of pet that needs to be updated", required = true, dataType = "string", paramType = "path"),
-    new ApiImplicitParam(name = "body", value = "Updated user object.", required = false, dataType = "string", paramType = "form")
+    new ApiImplicitParam(name = "id", value = "ID of the Node-Beacon be updated", required = true, dataType = "string", paramType = "path"),
+    new ApiImplicitParam(name = "body", value = "Updated node object.", required = false, dataType = "string", paramType = "form")
   ))
   @ApiResponses(Array(
-    new ApiResponse(code = 404, message = "User not found"),
-    new ApiResponse(code = 400, message = "Invalid username supplied")
+    new ApiResponse(code = 404, message = "Node not found"),
+    new ApiResponse(code = 400, message = "Invalid Node supplied")
   ))
-  def readRoute = put { path("user" / Segment) { id =>
+  def readRoute = put { path("beacon" / Segment) { id =>
     complete(s"Put ${id}")
   }}
 
-  @ApiOperation(value = "Get user by name", notes = "", response=classOf[User], nickname = "getUserByName", httpMethod = "GET")
+  @ApiOperation(value = "Get Noe by Id", notes = "", response=classOf[User], nickname = "getNodeByID", httpMethod = "GET")
   @ApiImplicitParams(Array(
-    new ApiImplicitParam(name = "petId", value = "ID of pet that needs to be updated", required = true, dataType = "string", paramType = "path"),
-    new ApiImplicitParam(name = "name", value = "Updated name of the pet.", required = false, dataType = "string", paramType = "form"),
-    new ApiImplicitParam(name = "status", value = "Updated status of the pet.", required = false, dataType = "string", paramType = "form")
+    new ApiImplicitParam(name = "Id", value = "ID of Node that needs to be updated", required = true, dataType = "string", paramType = "path")
   ))
   @ApiResponses(Array(
-    new ApiResponse(code = 404, message = "User does not exist.")
+    new ApiResponse(code = 404, message = "Node does not exist.")
   ))
-  def getBeacon = post { path("user" / Segment) { id => { formFields('name, 'status) { (name, status) =>
+  def getBeacon = post { path("beacon" / Segment) { id => { formFields('name, 'status) { (name, status) =>
     complete(s"Posted $name, $status")
   }}}}
 
 }
 
-case class Beacon(description: String, status: String)
+case class Beacon(id: String, description: String, status: String)
