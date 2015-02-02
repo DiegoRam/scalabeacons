5d444af0ab577860a4b4da7ac28092fbf9f30586
diegoram <ddarkheart@gmail.com>
2015-02-02 10:44:51 -0300

    refactor on memoryStorage

--- src/main/scala/com/teracode/beacons/services/memoryStorage.scala
+++ src/main/scala/com/teracode/beacons/services/memoryStorage.scala
@@ -2,16 +2,26 @@ package com.teracode.beacons.services
 
 import java.util.UUID
 
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
+
 /**
  * Created by diegoram on 2/1/15.
  */
 object MemoryStorage {
 
-  val beaconList = List(
+  val beaconList = ArrayBuffer[Beacon](
     Beacon(UUID.randomUUID(), "Fravega", "Retailer", "Active", List(Signal("Fravega-Wifi", 3), Signal("MacDonalsW", 2))),
     Beacon(UUID.randomUUID(), "MacDonals", "FastFood", "Active", List(Signal("MacDonalsW", 3), Signal("Fravega-Wifi", 2)))
   )
 
-  val userList = List()
+  var userList = List()
+
+  def addBeacon(beacon: Beacon): Try[Beacon] = {
+    Try{
+      beaconList += beacon
+      beacon
+    }
+  }
 
 }
