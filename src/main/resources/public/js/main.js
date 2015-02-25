19d95dba91ac6dabe7141a6636abd978a6a17c55
Matias Tealdi <matiastealdi@gmail.com>
2015-02-25 14:30:20 -0300

    adding code to tranform pixel point in a query.

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
 
-    var globalLocations = [{},{}];
+    var globalAPLocations = [{ssid: "AP1", x: 100, y: 315}, {ssid: "AP2", x:100, y: 90}, {ssid: "AP3", x:353, y: 90}, {ssid: "AP4", x:605, y: 90}];
+    var APmaxDistInPixeles = 280;
 
     $('#mall').click(function(e){
         var offset_t = $(this).offset().top - $(window).scrollTop();
@@ -12,20 +13,43 @@ $(document).ready(function(){
         $('#x').text('x: ' + left);
         $('#y').text('y: ' + top);
 
-        var query = createQuery(left, top);
+        var query = createQuery(globalAPLocations, {x: left, y: top});
         updateQueryPanel(query);
         callApi(query);
 
     });
 
-    function createQuery(x,y){
-        return {beacon: [{ssid: 'Leo Productions', level: 0.7}]};
+    /*
+      Take two points as dict with x and y keys and returns the
+      distance between p1 and p2 in the same scale
+    */
+    function distance(p1, p2) {
+        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+    }
+
+    function createQuery(apLocation, p){
+
+        var query = [];
+        for (var i=0; i<apLocation.length; i++) {
+            var dist = distance(apLocation[i], p);
+            if (dist < APmaxDistInPixeles) {
+                var signalLevel = 1.0 - dist / APmaxDistInPixeles;
+                query.push({ssid: apLocation[i].ssid, level: signalLevel});
+            }
+        }
+        return {beacons: query};
     }
 
     function callApi(query){
-        $.post("/locations/signal-search", {}, function(err, data){
-            showResults([{score: 0.766565},{score: 0.9987}]);
+        $.ajax({
+            type: "POST",
+            url: "/locations/signal-search",
+            processData: false,
+            contentType: 'application/json',
+            data: JSON.stringify(query),
+            success: function(data){ showResults(data); }
         });
+
     }
 
     function updateQueryPanel(query){
@@ -33,6 +57,6 @@ $(document).ready(function(){
     };
 
     function showResults(data){
-        $('#result').text(data);
+        $('#result').text(JSON.stringify(data.map(function (e) {return {score: e.score, name: e.source.name};})));
     };
 });
